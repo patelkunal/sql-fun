7a7b011d102a9d237eebc9d37df07f2abeae579f
Kunal <patelkunal89@gmail.com>
2015-06-15 23:12:34 +0530

    update with auo-incr and primary-key

--- apartment-rental-agency.sql
+++ apartment-rental-agency.sql
@@ -1,25 +1,29 @@
 create table Apartments
 (
-  AptId int(4),
+  AptId int(4) AUTO_INCREMENT,
   AptNo int(3),
-  BuildingId int(2)
+  BuildingId int(2),
+  PRIMARY KEY (AptId) 
 );
 
 create table Buildings
 (
-  BuildingId int(2),
+  BuildingId int(2) AUTO_INCREMENT,
   BuildingName varchar(100),
-  Address varchar(200)
+  Address varchar(200),
+  PRIMARY KEY (BuildingId)
 );
 
 create table Tenants
 (
-  TenantId int(2),
-  TenantName varchar(100)
+  TenantId int(2) AUTO_INCREMENT,
+  TenantName varchar(100),
+  PRIMARY KEY (TenantId)
 );
 
 create table AptTenants
 (
   TenantId int(2),
-  AptId int(2)
+  AptId int(2),
+  PRIMARY KEY (TenantId, AptId)
 );
