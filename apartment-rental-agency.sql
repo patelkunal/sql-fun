f949f39a66aac5e47fe434ae5de4ddf02b30cdff
Kunal <patelkunal89@gmail.com>
2015-06-16 10:53:11 +0530

    added composite primary key and foreign keys (wrong syntax)

--- apartment-rental-agency.sql
+++ apartment-rental-agency.sql
@@ -1,3 +1,11 @@
+create table Buildings
+(
+  BuildingId int(2) AUTO_INCREMENT,
+  BuildingName varchar(100),
+  Address varchar(200),
+  PRIMARY KEY (BuildingId)
+);
+
 create table Apartments
 (
   AptId int(4) AUTO_INCREMENT,
@@ -7,14 +15,6 @@ create table Apartments
   FOREIGN KEY (BuildingId) REFERENCES Buildings(BuildingId)
 );
 
-create table Buildings
-(
-  BuildingId int(2) AUTO_INCREMENT,
-  BuildingName varchar(100),
-  Address varchar(200),
-  PRIMARY KEY (BuildingId)
-);
-
 create table Tenants
 (
   TenantId int(2) AUTO_INCREMENT,
@@ -22,9 +22,12 @@ create table Tenants
   PRIMARY KEY (TenantId)
 );
 
+
 create table AptTenants
 (
-  TenantId int(2),
-  AptId int(2),
-  PRIMARY KEY (TenantId, AptId)
+  TenantId int(2) NOT NULL,
+  AptId int(2) NOT NULL,
+  PRIMARY KEY (TenantId, AptId),
+  FOREIGN KEY (TenantId) REFERENCES Tenants(TenantId),
+  FOREIGN KEY (AptId) REFERENCES Tenants(AptId)
 );
